101d14a6fb2be4f29d76c2d74bec6235535b343a
Martin Ben <mebenav88@gmail.com>
2015-12-04 16:46:02 -0500

    add controller coolctrl and imasibling

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,19 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function(){
     console.log("Hello there! This is the helloWorld controller function, in the mainCtrl");
   };
+})
+
+.controller('coolCtrl', function($scope){
+  $scope.whoAmI = function(){
+    console.log("hello there, this is the cool controller");
+  };
+
+  $scope.helloWorld = function(){
+    console.log('this is not the main controller');
+  };
+})
+
+.controller('imASibling',function($scope){
+  $scope.foobar = 1234;
+  // do other cool stuff!
 });
