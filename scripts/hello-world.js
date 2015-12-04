bcbf6c5b7ea8ac3dfc6d54125dff09038322fd0b
Martin Ben <mebenav88@gmail.com>
2015-12-04 15:48:13 -0500

    add restrict to skip element

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,6 +1,7 @@
 angular.module('todoListApp')
 .directive('helloWorld',function(){
   return {
-    template: "This is the hello world directive!"
+    template: "This is the hello world directive!",
+    restrict: "E"
   };
 });
