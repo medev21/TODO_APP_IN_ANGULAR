ef8d0feee2c675bfe0cfefcea6d9305c6a3b0e16
Martin Ben <mebenav88@gmail.com>
2016-03-30 16:13:18 -0400

    add ng change

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 angular.module("todoListApp", [])
 .controller("mainCtrl", function($scope){
-  $scope.helloWorld = function(){
-    console.log("Hello there! This is the helloWorld controller function, in the mainCtrl");
+  $scope.learningNgChange = function(){
+    console.log("an input changed");
   };
-
   $scope.todos = [
       {"name": "clean the house"},
       {"name": "water the dog"},
